eeeba699d63073acf832c99ed40746218a4b50a9
Arnav-Menon <arnavmenon@gmail.com>
2022-12-04 12:44:56 -0800

    done, maybe just some formatting left

--- nn.py
+++ nn.py
@@ -9,6 +9,8 @@ best_accuracy = 0
 
 def forwardSelection(data):
     current_set_of_features = []
+    accuracy = (leave_one_out_cv(data, current_set_of_features, []))*100
+    print(f"Accuracy on {current_set_of_features} is {accuracy:.1f}%")
     for i in range(1, len(data[0])):
         feature_to_add = 0
         best_so_far_accuracy = 0
@@ -16,7 +18,7 @@ def forwardSelection(data):
             if j not in current_set_of_features:
                 # maybe change j input to int and cast to list in loo_cv function
                 accuracy = (leave_one_out_cv(data, current_set_of_features, [j]))*100
-                # print(f"\tUsing feature(s) {current_set_of_features + [j]} accuracy is {accuracy:.1f}%")
+                print(f"\tUsing feature(s) {current_set_of_features + [j]} accuracy is {accuracy:.1f}%")
 
                 if accuracy > best_so_far_accuracy:
                     global best_accuracy
@@ -27,12 +29,14 @@ def forwardSelection(data):
                     best_so_far_accuracy = accuracy
                     feature_to_add = j
         current_set_of_features.append(feature_to_add)
-        print(f"\nFeature set {current_set_of_features} was best, accuracy is {best_so_far_accuracy:.1f}%")
+        print(f"Feature set {current_set_of_features} was best, accuracy is {best_so_far_accuracy:.1f}%")
 
     return [current_set_of_features, accuracy]
 
 def backwardElimination(data):
     current_set_of_features = [i for i in range(1, len(data[0]))]
+    accuracy = (leave_one_out_cv(data, current_set_of_features, []))*100
+    print(f"Accuracy on {current_set_of_features} is {accuracy:.1f}%")
     for i in range(1, len(data[0])):
         feature_to_remove = 0
         best_so_far_accuracy = 0
@@ -45,35 +49,26 @@ def backwardElimination(data):
 
                 if accuracy > best_so_far_accuracy:
                     global best_accuracy
-                    if accuracy < best_accuracy:
+                    if accuracy > best_accuracy:
                         best_accuracy = accuracy
                         global best_subset
-                        best_subset = current_set_of_features.remove(j)
+                        best_subset = current_set_of_features[:]
+                        best_subset.remove(j)
                     best_so_far_accuracy = accuracy
                     feature_to_remove = j
-        # print(f"Removing {feature_to_remove}")
         current_set_of_features.remove(feature_to_remove)
         print(f"Feature set {current_set_of_features} was best, accuracy is {best_so_far_accuracy:.1f}%")
-        # print(f"Current feature set {current_set_of_features}\n")
-        # print("-----------------------------")
-
-    # return [current_set_of_features, accuracy]
 
 def leave_one_out_cv(data, current_set, feature_add, be=False):
     # the [0] is to get the class labels along with the features
     if be:
-        # print(f"CS {current_set}")
-        # print(f"FA {feature_add}")
         # copy current_set into test and in different memory
         test = current_set[:]
         test.remove(feature_add)
-        # print(f"test {test}")
         data = data[:, [0] + test]
     else:
         data = data[:, [0] + current_set + feature_add]
 
-    # print(data)
-    # return random.randint(0, 10)
     number_correctly_identified = 0
     for i in range(len(data)):
         object_to_classify = data[i][1:]
@@ -84,9 +79,6 @@ def leave_one_out_cv(data, current_set, feature_add, be=False):
         for j in range(len(data)):
             if j != i:
                 distance = np.sqrt(np.sum(np.square((object_to_classify - data[j][1:]))))
-                # diff = object_to_classify - data[j][1:]
-                # distance = np.sqrt(np.sum(np.dot(diff, diff)))
-                # distance = np.sqrt(np.sum((object_to_classify - data[j][1:])**2))
                 if distance < nearest_neighbor_distance:
                     nearest_neighbor_distance = distance
                     nearest_neighbor_location = j
@@ -99,33 +91,26 @@ def leave_one_out_cv(data, current_set, feature_add, be=False):
     return accuracy
 
 if __name__ == "__main__":
+    test_file = str(input("What text file would you like to test? "))
+
+    algo = int(input("What algorithm would you like to run, 1 or 2? \n\t 1) Forward Selection \n\t 2) Backward Elimination\n"))
     start = time.time()
 
-    # df = np.loadtxt("eamonn_test_data.txt")
-    # df = np.loadtxt("eamonn_slides_data.txt")
+    df = np.loadtxt(test_file)
 
-    # df = np.loadtxt("small_test_data.txt")
+    print(f"This dataset has {len(df[0] )- 1} features, not including class attributes, with {len(df)} instances\n")
 
-    # df = np.loadtxt("small_96.txt")
-    # df = np.loadtxt("small_6.txt")
-    # df = np.loadtxt("small_88.txt")
+    # normalized_vector = df / np.linalg.norm(df)
+    # normalized_vector1 = df[1:] / np.linalg.norm(df)
 
-    # df = np.loadtxt("large_21.txt")
-    # df = np.loadtxt("large_96.txt")
-    # df = np.loadtxt("large_6.txt")
-
-    # my datasets
-    df = np.loadtxt("small_data.txt")
-    # df = np.loadtxt("large_data.txt")
-
-
-    print("Beginning forward search.")
-    forwardSelection(df)
-    # figure out accuracy decimal point stuff here
-    print(f"Finished forward search! The best feature subset is {best_subset}, which has an accuracy of {best_accuracy}%")
-    print(f"It took {time.time() - start:.1f} seconds.")
-
-    # print("Beginning backward search.")
-    # backwardElimination(df)
-    # print(f"Finished backward search! The best feature subset is {best_subset}, which has an accuracy of {best_accuracy}%")
-    # print(f"It took {time.time() - start:.1f} seconds.")
+    if algo == 1:
+        print("Beginning forward search.")
+        forwardSelection(df)
+        # figure out accuracy decimal point stuff here
+        print(f"Finished forward search! The best feature subset is {best_subset}, which has an accuracy of {best_accuracy:.1f}%")
+        print(f"It took {time.time() - start:.1f} seconds.")
+    else:
+        print("Beginning backward search.")
+        backwardElimination(df)
+        print(f"Finished backward search! The best feature subset is {best_subset}, which has an accuracy of {best_accuracy:.1f}%")
+        print(f"It took {time.time() - start:.1f} seconds.")
